1260d5cf7848c61a86c05b19226c9d1e41d65515
NickBragg <braggnickp@gmail.com>
2014-09-21 18:24:43 -0400

    Updated logic. Removed unneeded views.

--- app/src/main/java/com/example/patrick/rockpaperscissors/Result.java
+++ app/src/main/java/com/example/patrick/rockpaperscissors/Result.java
@@ -8,19 +8,80 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Date;
+import java.util.Random;
+
 
 public class Result extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = (TextView)findViewById(R.id.txtMessage);
-        ImageView imageView = (ImageView)findViewById(R.id.imageResult);
-        //imageView.setImageResource();
-        textView.setText("");
         setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        String userChoice = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String computerChoice = ComputerChoice();
+
+        TextView textView = (TextView)findViewById(R.id.txtMessage);
+
+        textView.setText(CompareChoices(userChoice,computerChoice));
+
+    }
+    private String CompareChoices(String user,String computer)
+    {
+        if(user == computer)
+        {
+            return "It's a tie";
+        }
+        else if (user == "rock"){
+            if(computer == "scissors")
+            {
+                return "Computer chose scissors. You win!";
+            }
+            else
+            {
+                return "Computer chose paper. You lost!";
+            }
+        }
+        else  if (user == "paper")
+        {
+            if(computer == "rock")
+            {
+                return "Computer chose rock. You win!";
+            }
+            else
+            {
+                return "Computer chose scissors. You lost!";
+            }
+        }
+        else
+        {
+            if(computer == "rock"){
+                return "Computer chose rock. You win!";
+            }
+            else
+            {
+                return "Computer chose paper. You lost!";
+            }
+        }
+    }
+    private String ComputerChoice()
+    {
+        ImageView imageView = (ImageView)findViewById(R.id.imageResult);
+
+        Random random = new Random(new Date().getTime()+1);
+        int randomNum = random.nextInt(3);
+        switch (randomNum) {
+            case 0:
+                imageView.setImageResource(R.drawable.rock);
+                return "rock";
+            case 1:
+                imageView.setImageResource(R.drawable.paper);
+                return "paper";
+            default:
+                imageView.setImageResource(R.drawable.scissors);
+                return "scissors";
+        }
     }
 
 
